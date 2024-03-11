b77a9944a46d63ee018903d7fbc8aab0d3d62528
Tim Brady <tfb414@gmail.com>
2024-03-11 09:27:54 -0600

    chore: removed some commented code

--- app/src/main/java/com/example/mctasky/MainActivity.kt
+++ app/src/main/java/com/example/mctasky/MainActivity.kt
@@ -1,52 +1,5 @@
 package com.example.mctasky
 
-//import android.os.Bundle
-//import android.widget.Button
-//import android.widget.TextView
-//import androidx.activity.enableEdgeToEdge
-//import androidx.appcompat.app.AppCompatActivity
-//import androidx.core.view.ViewCompat
-//import androidx.core.view.WindowInsetsCompat
-//import com.example.mctasky.data.TaskRepository
-//import com.example.mctasky.network.NetworkModule
-//import retrofit2.Call
-//import retrofit2.Callback
-//import retrofit2.Response
-//import retrofit2.Retrofit
-//import retrofit2.converter.gson.GsonConverterFactory
-
-//class MainActivity : AppCompatActivity() {
-//    private val taskRepository = TaskRepository()
-//    override fun onCreate(savedInstanceState: Bundle?) {
-//        super.onCreate(savedInstanceState)
-//        enableEdgeToEdge()
-//        setContentView(R.layout.activity_main)
-//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.getTasks)) { v, insets ->
-//            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-//            insets
-//        }
-//
-//        val getTasksButton: Button = findViewById(R.id.getTasks)
-//        getTasksButton.setOnClickListener {
-//            fetchTasks()
-//        }
-//    }
-//
-//    private fun fetchTasks() {
-//        taskRepository.fetchTasks(
-//            onSuccess = { tasks ->
-//                val taskTextView: TextView = findViewById(R.id.taskTextView)
-//                taskTextView.text = tasks.joinToString("\n") { it.task }
-//            },
-//            onError = { message ->
-//                val taskTextView: TextView = findViewById(R.id.taskTextView)
-//                taskTextView.text = message
-//            }
-//        )
-//    }
-//}
-
 import android.os.Bundle
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
